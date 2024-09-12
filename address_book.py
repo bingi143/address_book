8628a017cb9f770050743079aca476273fef5c74
bingi143 <bingivenkateswarlu578@gmail.com>
2024-09-12 11:51:54 +0530

    [Venkatesh] Add: Adding new contact to address book

--- address_book.py
+++ address_book.py
@@ -22,7 +22,6 @@ class Contacts:
         self.phone_number=phone_number
         self.email=email
 
-    
     def display(self):
         '''
           Description: 
@@ -32,28 +31,85 @@ class Contacts:
           Return : 
                 None
         '''
-        print(f"""first-name: {self.first_name}\n 
-              last-name: {self.last_name}\n 
-              address: {self.address}\n 
-              city: {self.city}\n 
-              state: {self.state}\n
-              zip-code: {self.zip_code}\n 
-              phone-number: {self.phone_number}\n 
-              email: {self.email}""")
+        print("****************************\n",
+              f" first-name: {self.first_name}\n",
+              f" last-name: {self.last_name}\n",
+              f" address: {self.address}\n", 
+              f" city: {self.city}\n ",
+              f" state: {self.state}\n",
+              f" zip-code: {self.zip_code}\n ",
+              f" phone-number: {self.phone_number}\n ",
+              f" email: {self.email}\n",
+              "************************")
+
+
+class AddressBook:
+    def __init__(self):
+        self.contacts_list = []
+
+    def add_contact(self, contact):
+        '''
+        Description: 
+            Adds a new contact to the address book.
+        Parameters: 
+            contact: Contact object to add.
+        Return: 
+            None
+        '''
+        self.contacts_list.append(contact)
+        print(f"Contact for {contact.first_name} {contact.last_name} added successfully.\n")
+
+
+    def display_all_contacts(self):
+        '''
+        Description: 
+            Displays all contacts in the address book.
+        Parameters: 
+            None
+        Return: 
+            None
+        '''
+        if not self.contacts_list:
+            print("Address Book is empty.\n")
+        else:
+            for contact in self.contacts_list:
+                contact.display()
 
 
 def main():
-    print("Welcome to address book program")
-    first_name=input("Enter first name:")
-    last_name=input("Enter last name:")
-    address=input("Enter address:")
-    city=input("Enter city:")
-    state=input("Enter state:")
-    zip_code=int(input("Enter zip-code:"))
-    phone_number=int(input("Enter phone number:"))
-    email=input("Enter email:")
-    person_1=Contacts(first_name,last_name,address,city,state,zip_code,phone_number,email)
-    person_1.display()
+
+    print("Welcome to the Address Book Program")
+    address_book = AddressBook()
+
+    while True:
+        print("\n1. Add New Contact")
+        print("2. Display All Contacts")
+        print("3. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            first_name = input("Enter first name: ")
+            last_name = input("Enter last name: ")
+            address = input("Enter address: ")
+            city = input("Enter city: ")
+            state = input("Enter state: ")
+            zip_code = input("Enter zip code: ")
+            phone_number = input("Enter phone number: ")
+            email = input("Enter email: ")
+
+            new_contact = Contacts(first_name, last_name, address, city, state, zip_code, phone_number, email)
+            address_book.add_contact(new_contact)
+
+        elif choice == '2':
+            address_book.display_all_contacts()
+
+        elif choice == '3':
+            print("Exiting Address Book.")
+            break
+
+        else:
+            print("Invalid choice, please try again.")
 
 
 if __name__=="__main__":
